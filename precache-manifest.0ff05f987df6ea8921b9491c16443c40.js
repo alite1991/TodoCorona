6dd83a9614ea033fc47551f314979d3d6be9fe76
Alireza Tehrani <alite1991@users.noreply.github.com>
2020-04-11 12:05:25 +0430

    Updates

---
+++ precache-manifest.0ff05f987df6ea8921b9491c16443c40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89cbe366a3e440ac1887df5e4583242f",
+    "url": "/TodoCorona/index.html"
+  },
+  {
+    "revision": "158ff4a4d76f5dce365a",
+    "url": "/TodoCorona/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "fa12f57fc9c32c9ac281",
+    "url": "/TodoCorona/static/js/2.55d41aad.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/TodoCorona/static/js/2.55d41aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158ff4a4d76f5dce365a",
+    "url": "/TodoCorona/static/js/main.bc50c4e3.chunk.js"
+  },
+  {
+    "revision": "390c21d3f2ebfdb38ae2",
+    "url": "/TodoCorona/static/js/runtime-main.4e64434a.js"
+  }
+]);
